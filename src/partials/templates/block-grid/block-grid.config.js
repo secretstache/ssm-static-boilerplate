8f16b42e636704000c0a6a3115d4ecc37282ae76
serhiigit <serhiivovnenko@gmail.com>
2024-04-22 09:41:30 +0300

    Block Grid Template

---
+++ src/partials/templates/block-grid/block-grid.config.js
@@ -0,0 +1,101 @@
+module.exports = {
+    title: 'Block Grid',
+    context: {
+        items: [
+            {
+                link: '#',
+                img: '/assets/images/cms/placeholder.svg',
+                categoryList: [
+                    {
+                        title: 'category 01'
+                    },
+                ],
+                headline: 'Title 01',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aliquam repudiandae, impedit in natus cupiditate perferendis reiciendis quaerat non veniam expedita a, modi necessitatibus! Ad nobis quisquam quae perferendis, cumque quod, placeat error id, delectus facere tempora fuga ex similique porro. Nihil obcaecati nesciunt quos repellendus fugit necessitatibus, voluptatum commodi!'
+            },
+            {
+                link: '#',
+                img: '/assets/images/cms/placeholder.svg',
+                categoryList: [
+                    {
+                        title: 'category 01',
+                    },
+                    {
+                        title: 'category 02',
+                    },
+                    {
+                        title: 'category 03',
+                    },
+                ],
+                headline: 'Title 02',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aliquam repudiandae, impedit in natus cupiditate perferendis reiciendis quaerat non veniam expedita a, modi necessitatibus! Ad nobis quisquam quae perferendis, cumque quod, placeat error id, delectus facere tempora fuga ex similique porro. Nihil obcaecati nesciunt quos repellendus fugit necessitatibus, voluptatum commodi!'
+            },
+            {
+                link: '#',
+                img: '/assets/images/cms/placeholder.svg',
+                categoryList: [
+                    {
+                        title: 'category 01'
+                    },
+                ],
+                headline: 'Title 03',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aliquam repudiandae, impedit in natus cupiditate perferendis reiciendis quaerat non veniam expedita a, modi necessitatibus! Ad nobis quisquam quae perferendis, cumque quod, placeat error id, delectus facere tempora fuga ex similique porro. Nihil obcaecati nesciunt quos repellendus fugit necessitatibus, voluptatum commodi!'
+            },
+            {
+                link: '#',
+                img: '/assets/images/cms/placeholder.svg',
+                categoryList: [
+                    {
+                        title: 'category 01'
+                    },
+                ],
+                headline: 'Title 04',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aliquam repudiandae, impedit in natus cupiditate perferendis reiciendis quaerat non veniam expedita a, modi necessitatibus! Ad nobis quisquam quae perferendis, cumque quod, placeat error id, delectus facere tempora fuga ex similique porro. Nihil obcaecati nesciunt quos repellendus fugit necessitatibus, voluptatum commodi!'
+            },
+            {
+                link: '#',
+                img: '/assets/images/cms/placeholder.svg',
+                categoryList: [
+                    {
+                        title: 'category 01'
+                    },
+                ],
+                headline: 'Title 05',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aliquam repudiandae, impedit in natus cupiditate perferendis reiciendis quaerat non veniam expedita a, modi necessitatibus! Ad nobis quisquam quae perferendis, cumque quod, placeat error id, delectus facere tempora fuga ex similique porro. Nihil obcaecati nesciunt quos repellendus fugit necessitatibus, voluptatum commodi!'
+            },
+            {
+                link: '#',
+                img: '/assets/images/cms/placeholder.svg',
+                categoryList: [
+                    {
+                        title: 'category 01'
+                    },
+                ],
+                headline: 'Title 06',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aliquam repudiandae, impedit in natus cupiditate perferendis reiciendis quaerat non veniam expedita a, modi necessitatibus! Ad nobis quisquam quae perferendis, cumque quod, placeat error id, delectus facere tempora fuga ex similique porro. Nihil obcaecati nesciunt quos repellendus fugit necessitatibus, voluptatum commodi!'
+            },
+            {
+                link: '#',
+                img: '/assets/images/cms/placeholder.svg',
+                categoryList: [
+                    {
+                        title: 'category 01'
+                    },
+                ],
+                headline: 'Title 07',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aliquam repudiandae, impedit in natus cupiditate perferendis reiciendis quaerat non veniam expedita a, modi necessitatibus! Ad nobis quisquam quae perferendis, cumque quod, placeat error id, delectus facere tempora fuga ex similique porro. Nihil obcaecati nesciunt quos repellendus fugit necessitatibus, voluptatum commodi!'
+            },
+            {
+                link: '#',
+                img: '/assets/images/cms/placeholder.svg',
+                categoryList: [
+                    {
+                        title: 'category 01'
+                    },
+                ],
+                headline: 'Title 08',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aliquam repudiandae, impedit in natus cupiditate perferendis reiciendis quaerat non veniam expedita a, modi necessitatibus! Ad nobis quisquam quae perferendis, cumque quod, placeat error id, delectus facere tempora fuga ex similique porro. Nihil obcaecati nesciunt quos repellendus fugit necessitatibus, voluptatum commodi!'
+            },
+        ],
+    },
+};
