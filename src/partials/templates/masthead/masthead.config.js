6d854b4be7cf53668c918cd049a2c8c00aeda7ac
serhiigit <serhiivovnenko@gmail.com>
2024-05-03 13:26:05 +0300

    Masthead

---
+++ src/partials/templates/masthead/masthead.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'Masthead',
+    context: {
+        innerContent: 'two-columns',
+    },
+    variants: [
+        {
+            title: 'Single Column',
+            context: {
+                innerContent: 'single'
+            },
+        },
+    ],
+};
