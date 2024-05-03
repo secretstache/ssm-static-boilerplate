37336ca2717f1505a6864129eeea6375cf69c7eb
YueJin <yuejin07@gmail.com>
2024-05-03 15:58:12 +0300

    Search Results

---
+++ src/partials/templates/search-result/search-result.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    title: 'Search Result',
+    context: {
+        templateHeader: {
+            headers: [
+                {
+                    tag: 'p',
+                    text: 'Search results for:',
+                },
+                {
+                    tag: 'h1',
+                    text: 'Lorem ipsum',
+                },
+            ],
+        },
+        backButton: '#',
+        items: [
+            {
+                link: '#',
+                title: 'Lorem ipsum dolor',
+                description: 'Lorem ipsum dolor sit amet consectetur. Quis lacus eget lorem nulla commodo a ut. Nibh mi ac est donec. Facilisi proin elementum nisi elementum nullam sagittis. In nibh sollicitudin in arcu aliquam...',
+            },
+            {
+                link: '#',
+                title: 'Lorem ipsum dolor',
+                description: 'Lorem ipsum dolor sit amet consectetur. Quis lacus eget lorem nulla commodo a ut. Nibh mi ac est donec. Facilisi proin elementum nisi elementum nullam sagittis. In nibh sollicitudin in arcu aliquam...',
+            },
+            {
+                link: '#',
+                title: 'Lorem ipsum dolor',
+                description: 'Lorem ipsum dolor sit amet consectetur. Quis lacus eget lorem nulla commodo a ut. Nibh mi ac est donec. Facilisi proin elementum nisi elementum nullam sagittis. In nibh sollicitudin in arcu aliquam...',
+            },
+        ],
+    },
+};
