677f0cda7e2f534109cc82114ced164c5d3fdc7d
serhiigit <serhiivovnenko@gmail.com>
2024-06-02 17:12:37 +0300

    Link List

---
+++ src/partials/templates/link-list/link-list.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    title: 'Link List',
+    context: {
+        items: [
+            {
+                title: 'Tile 01',
+                link: '#',
+                button: 'Button Title'
+            },
+            {
+                title: 'Tile 02',
+                link: '#',
+                button: 'Button Title'
+            },
+            {
+                title: 'Tile 03',
+                link: '#',
+                button: 'Button Title'
+            },
+            {
+                title: 'Tile 04',
+                link: '#',
+                button: 'Button Title'
+            },
+            {
+                title: 'Tile 05',
+                link: '#',
+                button: 'Button Title'
+            },
+            {
+                title: 'Tile 06',
+                link: '#',
+                button: 'Button Title'
+            },
+        ],
+    },
+};
