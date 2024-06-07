5c794217ea477daaf6941f4f8d3513abc1b9ffb0
serhiigit <serhiivovnenko@gmail.com>
2024-06-07 14:03:05 +0300

    Horizontal Tabs: js refactoring and optimization

--- src/assets/scripts/templates/horizontal-tabs.js
+++ src/assets/scripts/templates/horizontal-tabs.js
@@ -7,12 +7,15 @@ const INNER_CONTENT_SELECTOR = '.template-horizontal-tabs__content-inner';
 const CLOSE_BUTTON_SELECTOR = '.template-horizontal-tabs__close-button';
 const ACTIVE_CLASS = 'is-active';
 const VISIBLE_CONTENT_CLASS = 'is-visible-tab-content';
+const HAS_OPEN_TAB_CLASS = 'has-open-tab';
 const MOBILE_BREAKPOINT = 1024;
 
 export default function HorizontalTabs() {
     const setActive = (el) => {
-        [...el.parentElement.parentElement.children].forEach((sib) => sib.classList.remove(VISIBLE_CONTENT_CLASS));
-        [...el.parentElement.parentElement.children].forEach((sib) => sib.classList.remove(ACTIVE_CLASS));
+        const parent = el.parentElement.parentElement;
+        parent.querySelectorAll(`.${VISIBLE_CONTENT_CLASS}, .${ACTIVE_CLASS}`).forEach(sib => {
+            sib.classList.remove(VISIBLE_CONTENT_CLASS, ACTIVE_CLASS);
+        });
 
         el.parentElement.classList.add(ACTIVE_CLASS);
         setTimeout(() => {
@@ -20,7 +23,33 @@ export default function HorizontalTabs() {
         }, 600);
     };
 
-    document.querySelectorAll(BLOCK_SELECTOR).forEach((block) => {
+    const handleResize = (tabs, contentBlocks, tabsList) => {
+        if (window.innerWidth >= MOBILE_BREAKPOINT) {
+            contentBlocks.forEach(contentBlock => {
+                contentBlock.style.height = `auto`;
+            });
+
+            tabsList.classList.remove(HAS_OPEN_TAB_CLASS);
+
+            tabs.forEach(tab => {
+                tab.classList.remove(ACTIVE_CLASS);
+            });
+
+            tabs[tabs.length - 1].classList.add(ACTIVE_CLASS);
+            setTimeout(() => {
+                tabs[tabs.length - 1].classList.add(VISIBLE_CONTENT_CLASS);
+            }, 600);
+        } else {
+            tabs.forEach(tab => {
+                tab.classList.remove(ACTIVE_CLASS);
+            });
+            contentBlocks.forEach(contentBlock => {
+                contentBlock.style.height = 0;
+            });
+        }
+    };
+
+    document.querySelectorAll(BLOCK_SELECTOR).forEach(block => {
         const tabs = block.querySelectorAll(TAB_SELECTOR);
         const tabsList = block.querySelector(TAB_LIST_SELECTOR);
         const tabsButtons = block.querySelectorAll(TAB_LABEL_SELECTOR);
@@ -34,16 +63,16 @@ export default function HorizontalTabs() {
             tabs[tabs.length - 1].classList.add(VISIBLE_CONTENT_CLASS);
         }, 600);
 
-        tabsButtons.forEach((el) =>
-            el.addEventListener('click', (e) => {
-                const currentContentBlock = el.parentElement.querySelector(CONTENT_BLOCK_SELECTOR);
-                const contentInnerHeight = el.parentElement.querySelector(INNER_CONTENT_SELECTOR).offsetHeight;
-                const closeButton = el.parentElement.querySelector(CLOSE_BUTTON_SELECTOR);
+        tabsButtons.forEach(el => {
+            const currentContentBlock = el.parentElement.querySelector(CONTENT_BLOCK_SELECTOR);
+            const contentInnerHeight = el.parentElement.querySelector(INNER_CONTENT_SELECTOR).offsetHeight;
+            const closeButton = el.parentElement.querySelector(CLOSE_BUTTON_SELECTOR);
 
-                tabsList.classList.add('has-open-tab');
+            el.addEventListener('click', () => {
+                tabsList.classList.add(HAS_OPEN_TAB_CLASS);
 
                 closeButton.addEventListener('click', () => {
-                    tabsList.classList.remove('has-open-tab');
+                    tabsList.classList.remove(HAS_OPEN_TAB_CLASS);
                     currentContentBlock.style.height = 0;
                     el.parentElement.classList.remove(ACTIVE_CLASS);
                 });
@@ -53,7 +82,7 @@ export default function HorizontalTabs() {
                         currentContentBlock.style.height = 0;
                         el.classList.remove(ACTIVE_CLASS);
                     } else {
-                        contentBlocks.forEach((contentBlock) => {
+                        contentBlocks.forEach(contentBlock => {
                             contentBlock.style.height = 0;
                         });
                         currentContentBlock.style.height = `${contentInnerHeight}px`;
@@ -63,33 +92,9 @@ export default function HorizontalTabs() {
                     setActive(el);
                     currentContentBlock.style.height = `auto`;
                 }
-            }),
-        );
-
-        window.addEventListener('resize', (event) => {
-            if (window.innerWidth >= MOBILE_BREAKPOINT) {
-                contentBlocks.forEach((contentBlock) => {
-                    contentBlock.style.height = `auto`;
-                });
-
-                tabsList.classList.remove('has-open-tab');
-
-                tabs.forEach((tab) => {
-                    tab.classList.remove(ACTIVE_CLASS);
-                });
-
-                tabs[tabs.length - 1].classList.add(ACTIVE_CLASS);
-                setTimeout(() => {
-                    tabs[tabs.length - 1].classList.add(VISIBLE_CONTENT_CLASS);
-                }, 600);
-            } else {
-                tabs.forEach((tab) => {
-                    tab.classList.remove(ACTIVE_CLASS);
-                });
-                contentBlocks.forEach((contentBlock) => {
-                    contentBlock.style.height = 0;
-                });
-            }
+            });
         });
+
+        window.addEventListener('resize', () => handleResize(tabs, contentBlocks, tabsList));
     });
 }
