46fb9228c577e615540525dde0df2bda7afde7f7
serhiigit <serhiivovnenko@gmail.com>
2024-06-09 10:23:16 +0300

    Masonry Grid

---
+++ src/partials/templates/masonry-grid/masonry-grid.config.js
@@ -0,0 +1,94 @@
+module.exports = {
+    title: 'Masonry Grid',
+    context: {
+        class: 'has-2-columns',
+        items: [
+            {
+                title: 'title 01',
+                description: 'description'
+            },
+            {
+                title: 'title 02',
+                description: 'description'
+            },
+            {
+                title: 'title 03',
+                description: 'description'
+            },
+            {
+                title: 'title 04',
+                description: 'description'
+            },
+        ],
+    },
+    variants: [
+        {
+            title: 'Masonry Grid 3 columns',
+            context: {
+                class: 'has-3-columns',
+                items: [
+                    {
+                        title: 'title 01',
+                        description: 'description'
+                    },
+                    {
+                        title: 'title 02',
+                        description: 'description'
+                    },
+                    {
+                        title: 'title 03',
+                        description: 'description'
+                    },
+                    {
+                        title: 'title 04',
+                        description: 'description'
+                    },
+                    {
+                        title: 'title 05',
+                        description: 'description'
+                    },
+                    {
+                        title: 'title 06',
+                        description: 'description'
+                    },
+                ],
+            },
+        },
+        {
+            title: 'Masonry Grid 4 columns',
+            context: {
+                class: 'has-4-columns',
+                items: [
+                    {
+                        title: 'title 02',
+                        description: 'description'
+                    },
+                    {
+                        title: 'title 03',
+                        description: 'description'
+                    },
+                    {
+                        title: 'title 04',
+                        description: 'description'
+                    },
+                    {
+                        title: 'title 05',
+                        description: 'description'
+                    },
+                    {
+                        title: 'title 06',
+                        description: 'description'
+                    },
+                    {
+                        title: 'title 07',
+                        description: 'description'
+                    },
+                    {
+                        title: 'title 08',
+                        description: 'description'
+                    },
+                ],
+            },
+        },
+    ]
+};
