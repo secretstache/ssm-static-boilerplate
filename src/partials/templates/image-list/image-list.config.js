ed60bc94b698afcf77c87f1469d536d330c058e9
serhiigit <serhiivovnenko@gmail.com>
2024-06-30 14:14:25 +0300

    Image List

---
+++ src/partials/templates/image-list/image-list.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    title: 'Image List',
+    context: {
+        innerContent: 'default',
+        items: [
+            {
+                title: 'Title 01',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum perspiciatis quam mollitia! Consequuntur optio temporibus maxime autem sapiente ea facere.',
+                img: '/assets/images/cms/placeholder.svg',
+            },
+            {
+                title: 'Title 02',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum perspiciatis quam mollitia! Consequuntur optio temporibus maxime autem sapiente ea facere.',
+                img: '/assets/images/cms/placeholder.svg',
+            },
+            {
+                title: 'Title 03',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum perspiciatis quam mollitia! Consequuntur optio temporibus maxime autem sapiente ea facere.',
+                img: '/assets/images/cms/placeholder.svg',
+            },
+            {
+                title: 'Title 04',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum perspiciatis quam mollitia! Consequuntur optio temporibus maxime autem sapiente ea facere.',
+                img: '/assets/images/cms/placeholder.svg',
+            },
+            {
+                title: 'Title 05',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum perspiciatis quam mollitia! Consequuntur optio temporibus maxime autem sapiente ea facere.',
+                img: '/assets/images/cms/placeholder.svg',
+            },
+        ],
+    },
+};
