6946821670e1f25963537fbfc2e37cd4c0aac469
serhiigit <serhiivovnenko@gmail.com>
2024-08-18 17:51:47 +0300

    Template Sticky Section

---
+++ src/assets/scripts/templates/sticky-section.js
@@ -0,0 +1,32 @@
+const TEMPLATE_SELECTOR = '.template-sticky-section';
+const FIXED_CLASS = 'is-fixed';
+
+export default function StickySection() {
+    const templates = document.querySelectorAll(TEMPLATE_SELECTOR);
+
+    if (!templates.length) return;
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0
+    };
+
+    const observerCallback = (entries) => {
+        entries.forEach((entry) => {
+            const template = entry.target;
+
+            if (entry.isIntersecting) {
+                return
+            } else {
+                template.classList.add(FIXED_CLASS);
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+    templates.forEach((template) => {
+        observer.observe(template);
+    });
+}
---
+++ src/partials/templates/sticky-section/sticky-section.config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    title: 'Sticky Section',
+    context: {
+        img: '/assets/images/cms/placeholder.svg'
+    },
+};
