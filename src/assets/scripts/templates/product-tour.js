05ede536d37381d85f03f271720864ed34901a24
serhiigit <serhiivovnenko@gmail.com>
2024-08-18 17:51:23 +0300

    Product Tour: Optimization scripts

--- src/assets/scripts/templates/product-tour.js
+++ src/assets/scripts/templates/product-tour.js
@@ -3,7 +3,7 @@ import Splide from '@splidejs/splide';
 const BLOCK_SELECTOR = '.template-product-tour';
 const SLIDER_CLASS = '.splide';
 const PROGRESS_BAR_CLASS = '.template-product-tour__progress-bar';
-const CUREENT_ITEM_NUMBER_CLASS = '.template-product-tour__progress-current';
+const CURRENT_ITEM_NUMBER_CLASS = '.template-product-tour__progress-current';
 
 export default function ProductTour() {
     document.querySelectorAll(BLOCK_SELECTOR).forEach((block) => {
@@ -20,16 +20,15 @@ export default function ProductTour() {
 
         const splide = new Splide(carousel, options);
 
-        // progress bar
         const progressBar = block.querySelector(PROGRESS_BAR_CLASS);
-        const currentSlideEl = block.querySelector(CUREENT_ITEM_NUMBER_CLASS);
+        const currentSlideEl = block.querySelector(CURRENT_ITEM_NUMBER_CLASS);
 
-        splide.on('mounted move', function () {
+        splide.on('mounted move', () => {
             const end = splide.Components.Controller.getEnd() + 1;
             const rate = Math.min((splide.index + 1) / end, 1);
 
-            progressBar.style.width = String(100 * rate) + '%';
-            currentSlideEl.innerHTML = String(splide.index + 1).padStart(2, '0');
+            progressBar.style.width = `${(100 * rate).toFixed(2)}%`;
+            currentSlideEl.textContent = String(splide.index + 1).padStart(2, '0');
         });
 
         splide.mount();
