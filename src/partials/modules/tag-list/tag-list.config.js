acc24376df97cae5e614b96bd1255c03b0a2ba9e
serhiigit <serhiivovnenko@gmail.com>
2024-08-25 20:23:37 +0300

    Teg List: Add module

---
+++ src/partials/modules/tag-list/tag-list.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    title: 'Tag List',
+    context: {
+        items: [
+            {
+                title: 'item 01'
+            },
+            {
+                title: 'item 02'
+            },
+            {
+                title: 'item 03'
+            },
+            {
+                title: 'item 04'
+            },
+            {
+                title: 'item 05'
+            },
+            {
+                title: 'item 06'
+            },
+            {
+                title: 'item 07'
+            },
+        ]
+    },
+};
