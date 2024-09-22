afdee831f7b958b09d01904902d1ec85df7d2074
serhiigit <serhiivovnenko@gmail.com>
2024-09-22 22:51:13 +0300

    Subnav Template

---
+++ src/partials/templates/subnav/subnav.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    title: 'Subnav',
+    context: {
+        title: 'Headline',
+        items: [
+            {
+                link: '#',
+                title: 'Link Item 01'
+            },
+            {
+                link: '#',
+                title: 'Link Item 02'
+            },
+            {
+                link: '#',
+                title: 'Link Item 03'
+            },
+            {
+                link: '#',
+                title: 'Link Item 04'
+            },
+            {
+                link: '#',
+                title: 'Link Item 05'
+            },
+        ],
+    },
+};
