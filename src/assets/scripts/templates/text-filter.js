d843cfce3e4395dfca8f726bd95d0088509bdc5d
serhiigit <serhiivovnenko@gmail.com>
2024-10-20 19:57:48 +0300

    Text Filter: add has button variant, add no-result messege

--- src/assets/scripts/templates/text-filter.js
+++ src/assets/scripts/templates/text-filter.js
@@ -3,25 +3,54 @@ const INPUT_SELECTOR = '.template-text-filter__filter-input';
 const ITEMS_LIST_SELECTOR = '.template-text-filter__item-list';
 const ITEMS_ELEMENTS_SELECTOR = 'li';
 const HIDDEN_CLASS = 'hidden';
+const SEARCH_BUTTON_SELECTOR = '.template-text-filter__search-button';
+const NO_RESULTS_HTML = `<p>Result Not Found</p>`;
 
 export const TextFilter = () => {
     document.querySelectorAll(TEMPLATE_SELECTOR).forEach((template) => {
         const filterInput = template.querySelector(INPUT_SELECTOR);
         const itemList = template.querySelector(ITEMS_LIST_SELECTOR);
         const items = itemList.querySelectorAll(ITEMS_ELEMENTS_SELECTOR);
-
-        filterInput.addEventListener('keyup', function () {
+        const searchButton = template.querySelector(SEARCH_BUTTON_SELECTOR);
+        
+        const filterItems = () => {
             const filterValue = filterInput.value.toLowerCase();
+            let hasResults = false;
 
-            for (let i = 0; i < items.length; i++) {
-                const itemText = items[i].textContent.toLowerCase();
+            items.forEach((item) => {
+                const itemText = item.textContent.toLowerCase();
 
-                if (itemText.includes(filterValue)) {
-                    items[i].classList.remove(HIDDEN_CLASS);
+                if (filterValue === '') {
+                    item.classList.remove(HIDDEN_CLASS);
+                    hasResults = true;
+                } else if (itemText.includes(filterValue)) {
+                    item.classList.remove(HIDDEN_CLASS);
+                    hasResults = true;
                 } else {
-                    items[i].classList.add(HIDDEN_CLASS);
+                    item.classList.add(HIDDEN_CLASS);
                 }
+            });
+
+            const noResultItem = itemList.querySelector('.no-results');
+            if (noResultItem) {
+                noResultItem.remove();
             }
-        });
+
+            if (!hasResults && filterValue !== '') {
+                const noResult = document.createElement('li');
+                noResult.classList.add('no-results');
+                noResult.innerHTML = NO_RESULTS_HTML;
+                itemList.appendChild(noResult);
+            }
+        };
+
+        if (searchButton) {
+            searchButton.addEventListener('click', (event) => {
+                event.preventDefault();
+                filterItems();
+            });
+        } else {
+            filterInput.addEventListener('keyup', filterItems);
+        }
     });
 };
--- src/partials/templates/text-filter/text-filter.config.js
+++ src/partials/templates/text-filter/text-filter.config.js
@@ -58,4 +58,68 @@ module.exports = {
             },
         ],
     },
+    variants: [
+        {
+            title: 'Text Filter Has Button',
+            context: {
+                buttonSearch: true,
+                items: [
+                    {
+                        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                    },
+                    {
+                        text: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                    },
+                    {
+                        text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris',
+                    },
+                    {
+                        text: 'Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+                    },
+                    {
+                        text: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
+                    },
+                    {
+                        text: 'Qui officia deserunt mollit anim id est laborum sed ut.',
+                    },
+                    {
+                        text: 'Perspiciatis unde omnis iste natus error sit voluptatem accusantium.',
+                    },
+                    {
+                        text: 'Illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+                    },
+                    {
+                        text: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.',
+                    },
+                    {
+                        text: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet.',
+                    },
+                    {
+                        text: 'Consectetur adipisci velit sed quia non numquam eius modi tempora.',
+                    },
+                    {
+                        text: 'Incidunt ut labore et dolore magnam aliquam quaerat voluptatem.',
+                    },
+                    {
+                        text: 'Ut enim ad minima veniam quis nostrum exercitationem ullam corporis.',
+                    },
+                    {
+                        text: 'Suspendisse in est ante in nibh mauris cursus mattis molestie.',
+                    },
+                    {
+                        text: 'Facilisi morbi tempus iaculis urna id volutpat lacus laoreet non.',
+                    },
+                    {
+                        text: 'Quis hendrerit dolor magna eget est lorem ipsum dolor sit.',
+                    },
+                    {
+                        text: 'Nulla pharetra diam sit amet nisl suscipit adipiscing bibendum est.',
+                    },
+                    {
+                        text: 'Vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci.',
+                    },
+                ],
+            },
+        }
+    ]
 };
